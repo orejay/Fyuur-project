62693a9a17756e21e6a8b93f29341e038221858a
Ore Jaiyesimi <orejay2017@gmail.com>
2022-08-15 08:14:39 +0100

    complete

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ moment = Moment(app)
 app.config.from_object('config')
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
 
 
